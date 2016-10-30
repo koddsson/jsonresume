a501d1638c3fca66d6a53a0dec93b15f1b47be1e
Kristján Oddsson <koddsson@gmail.com>
2016-10-30 14:54:36 -0700

    Add a method for creating resumes

---
+++ functions/add-resume/index.js
@@ -0,0 +1,13 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({region: 'eu-west-1'});
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handle = (event, context, callback) => {
+  // Take the payload and put it into DynamoDB?
+  console.log(event);
+  console.log(context);
+  callback({
+    event, context
+  });
+}
