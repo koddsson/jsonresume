1eb3bef1749eef95048a34717f2a54a11d71375b
Kristján Oddsson <koddsson@gmail.com>
2016-10-30 15:24:57 -0700

    Return the actual validation errors and not the text

--- functions/add-resume/index.js
+++ functions/add-resume/index.js
@@ -20,7 +20,7 @@ exports.handle = (event, context, callback) => {
     console.log(valid);
 
     if (!valid) {
-      callback(ajv.errorsText());
+      callback(ajv.errors);
     } else {
       // Add it into dynamodb
       // (echo it back for now)
