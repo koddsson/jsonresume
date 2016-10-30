b692155c598b8fbbbe788ab2d1c56973b400cecb
Kristján Oddsson <koddsson@gmail.com>
2016-10-30 15:12:00 -0700

    Validate the data sent in against the schema

--- functions/add-resume/index.js
+++ functions/add-resume/index.js
@@ -1,14 +1,27 @@
+const fs = require('fs');
 const AWS = require('aws-sdk');
+const Ajv = require('ajv');
+const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
 
 AWS.config.update({region: 'eu-west-1'});
 const docClient = new AWS.DynamoDB.DocumentClient();
 
+// TODO: Convert to promises?
 exports.handle = (event, context, callback) => {
-  // Take the payload and put it into DynamoDB?
-  console.log(event);
-  console.log(context);
-  callback(
-    null,
-    {event, context}
-  );
+  fs.readFile('./schema.json', (err, schema) => {
+    if (err) {
+      callback(err);
+    }
+
+    // Validate the payload (event) against the JSON schema
+    const valid = ajv.validate(schema, event);
+
+    if (!valid) {
+      callback(ajv.errorsText());
+    } else {
+      // Add it into dynamodb
+      // (echo it back for now)
+      callback(null, event);
+    }
+  });
 }
