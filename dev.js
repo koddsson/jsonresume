11c865c3380f3a1a9ec0ab9abacf483b9f798653
Kristján Oddsson <kristjan@cloudflare.com>
2017-06-01 23:09:50 +0100

    feat: take in filename in development

--- dev.js
+++ dev.js
@@ -4,9 +4,11 @@ const mustache = require("mustache");
 
 const app = express();
 
+const file = process.argv[2] || "stebbib.json";
+
 app.get("/", function(req, res) {
   fs.readFile("./themes/resume.template", (templateError, template) => {
-    fs.readFile("./test_documents/stebbib.json", (err, data) => {
+    fs.readFile(`./test_documents/${file}`, (err, data) => {
       const renderedData = mustache.render(
         template.toString(),
         JSON.parse(data.toString())
