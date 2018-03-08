9c54ae6707a192ff61ac051c74611cb0fd825a63
Kristján Oddsson <koddsson@gmail.com>
2018-03-08 23:28:07 +0000

    aws-sdk uses callbacks and not promises

--- __mocks__/aws-sdk.js
+++ __mocks__/aws-sdk.js
@@ -2,7 +2,9 @@ const AWS = jest.genMockFromModule('aws-sdk')
 
 AWS.S3 = function() {
   return {
-    putObject: () => {}
+    putObject: (data, callback) => {
+      callback(null, true)
+    }
   }
 }
 
