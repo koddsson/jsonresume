3de16bab445fff3cec00dfaa47e7ca6a17c0b18b
Kristján Oddsson <koddsson@gmail.com>
2018-03-11 19:55:48 +0000

    convert eslint to JS

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+	env: {
+		es6: true,
+		node: true,
+		jest: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+	},
+	extends: ['eslint:recommended', 'prettier'],
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				trailingComma: 'all',
+				semi: false,
+				useTabs: true,
+			},
+		],
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+	},
+}
