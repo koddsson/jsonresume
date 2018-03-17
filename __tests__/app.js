89687d4f7b48dce8cd360f8801ec62ef198b7f95
Kristján Oddsson <koddsson@gmail.com>
2018-03-17 23:57:24 +0000

    fix tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,9 +4,6 @@ const app = require('../app')
 describe('/', () => {
 	test('should redirect to the homepage', async () => {
 		const response = await request(app).get('/')
-		expect(response.statusCode).toBe(302)
-		expect(response.headers.location).toBe(
-			'https://github.com/koddsson/jsonresume-monorepo',
-		)
+		expect(response.statusCode).toBe(200)
 	})
 })
