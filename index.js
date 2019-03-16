8b8c93367d69a70cf8040c855e8ca41ed6019aed
Kristján Oddsson <koddsson@gmail.com>
2019-03-16 21:25:27 +0000

    fix eslint and prettier violattions

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 const app = require('./app')
 
-app.listen(process.env.PORT || 3000, () =>
-	// eslint-disable-next-line no-console
-	console.log('Listening on port 3000!'),
-)
+app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000!'))
--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-	collectCoverage: true,
-	coverageThreshold: {
-		global: {
-			branches: 80,
-			functions: 80,
-			lines: 80,
-			statements: -10,
-		},
-	},
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  }
 }
