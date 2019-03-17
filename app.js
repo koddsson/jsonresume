83747327a6e3e057d4946e334fc33c2ce1215904
Kristján Oddsson <koddsson@gmail.com>
2019-03-17 10:12:19 +0000

    Revert "try redirecting to HTTPS again"
    This reverts commit 7255bcb1eeac9de5e365d954503496e85a48ccd3.

--- app.js
+++ app.js
@@ -10,13 +10,6 @@ const getResume = require('./lib/get-resume')
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(function(req, res, next) {
-  if (!req.secure) {
-    return res.redirect(`https://${req.headers.host}${req.url}`)
-  }
-  next()
-})
-
 app.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/site/index.html`)))
 
 app.use(express.static('public'))
