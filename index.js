2f3fe70a6c23e172b6836e103fcdc7b5304d22e0
Kristján Oddsson <koddsson@gmail.com>
2019-03-17 10:08:10 +0000

    console log correct port on boot

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const app = require('./app')
 
-app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000!'))
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
